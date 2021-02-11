45e5fc90908df0a5aefc6dfcca552c222da4fcc3
Laxgrie <gabrielchesney@gmail.com>
2021-02-11 12:22:25 -0500

    Began customizing admin interface

--- display/admin.py
+++ display/admin.py
@@ -6,7 +6,9 @@ class RoomAdmin(admin.ModelAdmin):
     pass
 
 class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'group', 'room', 'date', 'timeStart')
     pass
 
 admin.site.register(Room, RoomAdmin)
 admin.site.register(Event, EventAdmin)
+admin.site.site_header = "Morehead State University SCES Digital Signage & Room Management"
--- display/models.py
+++ display/models.py
@@ -3,6 +3,8 @@ from django.db import models
 # Create your models here.
 class Room(models.Model):
     id = models.CharField(max_length=20, primary_key=True)
+    def __str__(self):
+        return self.id
 
 class Event(models.Model):
     name = models.CharField(max_length=20)
