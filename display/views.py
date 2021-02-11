1d99583f9e884b9f2c19c32a88f675405b4e5a92
Laxgrie <gabrielchesney@gmail.com>
2021-02-11 13:36:56 -0500

    Further refined Display query

--- display/views.py
+++ display/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from display.models import *
 from datetime import date
+from django.db.models import Count, Min, Subquery
 
 # Create your views here.
 def display(request):
     rooms = Room.objects.all()
-    #events = Event.objects.raw('SELECT * FROM display_event WHERE room IN (SELECT max(room) FROM display_event GROUP BY name)')
-    #events = Event.objects.all().order_by().values("room").distinct()
-    events = Event.objects.all()
+    
+    events_distinct = Event.objects.filter(date=date.today()).order_by('-timeStart').values('room').distinct().annotate(occurrences=Count('room'), pk=Min('pk'))
+    events = Event.objects.filter(id__in=Subquery(events_distinct.values('pk')))
     context = {
         "rooms" : rooms,
         "events" : events,
