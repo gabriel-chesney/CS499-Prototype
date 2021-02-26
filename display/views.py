5122fcdb4bc705a18230dc12a8b43bb59956fca3
Laxgrie <gabrielchesney@gmail.com>
2021-02-26 11:03:20 -0500

    tampered with queryset for display

--- display/views.py
+++ display/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from display.models import *
-from datetime import date
-from django.db.models import Count, Min, Subquery
+from datetime import date, datetime
+from django.db.models import Count, Min, Subquery, Max
 
 # Create your views here.
 def display(request):
     rooms = Room.objects.all()
     
-    events_distinct = Event.objects.filter(date=date.today()).order_by('-timeStart').values('room').distinct().annotate(occurrences=Count('room'), pk=Min('pk'))
-    events = Event.objects.filter(id__in=Subquery(events_distinct.values('pk')))
+    events = Event.objects.filter(date=date.today()).filter(timeEnd__gte=datetime.now())
+
+    #events_distinct = Event.objects.filter(date=date.today()).order_by('-timeStart').values('room').distinct().annotate(occurrences=Count('room'), pk=Max('pk'))
+    #events = Event.objects.filter(id__in=Subquery(events_distinct.values('pk')))
     context = {
         "rooms" : rooms,
         "events" : events,
