b49275c5161a75aade310da77ea97391bd73d47e
Laxgrie <gabrielchesney@gmail.com>
2021-03-31 10:46:43 -0400

    Configured link to admin page

--- display/urls.py
+++ display/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
 from display import views
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('', views.display, name = 'display')
+    path('', views.display, name = 'display'),
+    path(r"admin/", admin.site.urls, name="admin")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
